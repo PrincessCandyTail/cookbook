e541638652888fd6743aade0a43dda8f6a1a6d44
PrincessCandyTail <schaer_simon@gmx.ch>
2024-07-15 10:01:39 +0200

    neu

---
+++ frontend/cookbook-frontend/src/services/api.ts
@@ -0,0 +1,39 @@
+const API_URL = 'http://localhost:8080/api';
+
+async function fetchAPI(endpoint: string) {
+    const res = await fetch(`${API_URL}${endpoint}`);
+
+    if (!res.ok) {
+        const error = await res.json();
+        throw new Error(error.message);
+    }
+    return res.json();
+}
+
+export async function fetchBooks() {
+    return fetchAPI('/books');
+}
+
+export async function fetchUsers() {
+    return fetchAPI('/users');
+}
+
+export async function fetchGroups() {
+    return fetchAPI('/groups');
+}
+
+export async function fetchRecipes() {
+    return fetchAPI('/recipes');
+}
+
+export async function fetchIngredients() {
+    return fetchAPI('/ingredients');
+}
+
+export async function fetchDescriptions() {
+    return fetchAPI('/descriptions');
+}
+
+export async function fetchUnits() {
+    return fetchAPI('/units');
+}
