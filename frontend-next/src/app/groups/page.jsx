e3611f3984817c93ada45bcd15a84f159e0b0b35
PrincessCandyTail <schaer_simon@gmx.ch>
2024-07-17 09:40:48 +0200

    fetch in groups

--- frontend-next/src/app/groups/page.jsx
+++ frontend-next/src/app/groups/page.jsx
@@ -1,13 +1,42 @@
-'use client'
-import Header from '../../components/Header';
-import GroupList from '../../components/GroupList';
+'use client';
+
 import { useEffect, useState } from 'react';
+import Header from "@/components/Header";
+import GroupList from "@/components/GroupList";
 
 export default function GroupsPage() {
     const [groups, setGroups] = useState([]);
 
     useEffect(() => {
-        // fetch
+
+        const fetchGroups = async () => {
+            const token = "eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJzZGFhIiwiaWF0IjoxNzIxMjAwMjkzLCJleHAiOjE3MjEyODY2OTN9.ZRArIjePyWYm6VLbWgZRVmT04V6QEo4cQtEFHwC1RlXQoYpWb8ODPT2s70yMLWd9"
+            //localStorage.getItem('token');
+
+            if (!token) {
+                console.error('No token found');
+                return;
+            }
+
+            const myHeaders = new Headers();
+            myHeaders.append('Authorization', `Bearer ${token}`);
+
+            const requestOptions = {
+                method: 'GET',
+                headers: myHeaders,
+                redirect: 'follow'
+            };
+
+            try {
+                const response = await fetch('http://localhost:8080/api/groups', requestOptions);
+                const result = await response.json();
+                setGroups(result.groups);
+            } catch (error) {
+                console.error('Error fetching groups:', error);
+            }
+        };
+
+        fetchGroups();
     }, []);
 
     return (
