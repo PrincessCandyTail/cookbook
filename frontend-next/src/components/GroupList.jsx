612a3ecbcbb947748027a037763a978bc2c46537
PrincessCandyTail <schaer_simon@gmx.ch>
2024-07-17 15:56:57 +0200

    group list

--- frontend-next/src/components/GroupList.jsx
+++ frontend-next/src/components/GroupList.jsx
@@ -3,9 +3,16 @@ import GroupItem from "./GroupItem";
 export default function GroupList({ groups }) {
     return (
         <div>
-            {groups.map(group => (
+            {/*groups.map(group => (
                 <GroupItem key={group.id} group={group} />
-            ))}
+            )) */}
+
+            {
+
+                groups.map( group =>
+               <GroupItem group={group}/>
+                )
+            }
         </div>
     );
 };
